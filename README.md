73b2d829bf40d2f9fd575c70c3114d9791a465ff
nax <pokenachop@gmail.com>
2024-11-04 11:24:43 +0100

    add quickcheck radio cheat and DONETODO.md
    For not needing to use Debug mode, just grant me good pokes, badges and such, basically copied from Xaerochill's timecrystal

--- README.md
+++ README.md
@@ -2,60 +2,7 @@
 
 El objetivo es pulir el juego mediante muchas de las opciones explicadas en los tutoriales y así tener una base para otros proyectos derivados.
 
-### Hecho
-
-  remove pokémon sprite animations 31/08/2024
-  editar el readme.md con detalles del proyecto
-
-### Por hacer (más ! = mayor importancia)
-
-  ? traducirlo todo
-  !!! fix known bugs and maybe improve some messy code
-  !!! add debug menu
-  
-  !!! add a new type (fairy)
-  ! add a new pack pocket
-  
-  !! expand tilesets from 192 to 255
-  ! allow priority colours
-  allow tiles to have different attributes including x y flip - reduced tilesets?
-  colour party menu icons by species
-  allow more trainer parties with specifities
-  print text when you lose a trainer battle
-  !! short beeping noise from low HP
-  ! remove artificial save delay
-  ! acortar la duración del script de curación en centro pokémon
-  option to show shiny colours in pokédex
-  ? make wild encounters vary
-  !! modify existing gender formula - so female pokémon are not necessarily weaker lol
-  display more information on the move screen
-
-  remove failure chance for AI
-  ? remove redundant move grammar
-  ! remove gym badges boosts
-
-  !!! implement physical/special split
-  ! replace stat exp with dvs
-  no longer gain exp at level 100
-  ! show move names for tm/hm when receiving or buying
-  !! reusable tms
-  !!! running shoes
-  tweak sandstorm and hail behaviours
-  use unic colours for each pokeball
-  smashing rocks or headbutting trees may give items
-  make grass-type mons immune to powders
-  reviving pokémon from fossils
-  add move reminder
-  allow fishing while surfing
-
-  ! changing switch as an evolutionary method
-  disable jumping from ledges onto obstacles
-  !!! allow using a field move if the pokémon can learn it
-  ! toggable items like repel or healing from party menu
-
-  ! cambiar las unidades de medida imperiales por sistema métrico
-  ! experimentar con las opciones de player - género, colores...
-  ! implementar el plano aéreo para sustituir el tipo Volador o la habilidad Levitación
+Para la lista de cambios, ver [DONETODO.md](DONETODO.md).
   
 # Pokémon Crystal [![Build Status][ci-badge]][ci]
 
--- maps/PlayersHouse2F.asm
+++ maps/PlayersHouse2F.asm
@@ -50,23 +50,108 @@ PlayersHousePosterScript:
 	describedecoration DECODESC_POSTER
 
 PlayersHouseRadioScript:
-	checkevent EVENT_GOT_A_POKEMON_FROM_ELM
-	iftrue .NormalRadio
-	checkevent EVENT_LISTENED_TO_INITIAL_RADIO
-	iftrue .AbbreviatedRadio
-	playmusic MUSIC_POKEMON_TALK
-	opentext
-	writetext PlayersRadioText1
-	pause 45
-	writetext PlayersRadioText2
-	pause 45
-	writetext PlayersRadioText3
-	pause 45
-	musicfadeout MUSIC_NEW_BARK_TOWN, 16
-	writetext PlayersRadioText4
-	pause 45
+		opentext
+	; from https://github.com/xaerochill/timecrystal/commit/7a0eb1f8a9ae1168c0a2e384a45d26d06d1deb1e#diff-0b7d6896c2a1908426662abbc1d82bd12e8b70e86c1791d7c49c97ca84d5ce09R48
+	; except for e4 cleared
+	; unlock stationary legendaries
+	setevent ENGINE_UNLOCKED_UNOWNS_A_TO_K
+	; full pokegear
+	setflag ENGINE_POKEGEAR
+	setflag ENGINE_PHONE_CARD
+	setflag ENGINE_MAP_CARD
+	setflag ENGINE_RADIO_CARD
+	setflag ENGINE_EXPN_CARD
+	; pokedex
+	setflag ENGINE_POKEDEX
+	; useful items
+	giveitem MAX_REPEL, 99
+	giveitem MASTER_BALL, 99
+	; all badges
+	setflag ENGINE_ZEPHYRBADGE
+	setflag ENGINE_HIVEBADGE
+	setflag ENGINE_PLAINBADGE
+	setflag ENGINE_FOGBADGE
+	setflag ENGINE_STORMBADGE
+	setflag ENGINE_MINERALBADGE
+	setflag ENGINE_GLACIERBADGE
+	setflag ENGINE_RISINGBADGE
+	setflag ENGINE_BOULDERBADGE
+	setflag ENGINE_CASCADEBADGE
+	setflag ENGINE_THUNDERBADGE
+	setflag ENGINE_RAINBOWBADGE
+	setflag ENGINE_MARSHBADGE
+	setflag ENGINE_SOULBADGE
+	setflag ENGINE_VOLCANOBADGE
+	setflag ENGINE_EARTHBADGE
+	setevent EVENT_BEAT_FALKNER
+	setevent EVENT_BEAT_BUGSY
+	setevent EVENT_BEAT_WHITNEY
+	setevent EVENT_BEAT_MORTY
+	setevent EVENT_BEAT_CHUCK
+	setevent EVENT_BEAT_JASMINE
+	setevent EVENT_BEAT_PRYCE
+	setevent EVENT_BEAT_CLAIR
+	setevent EVENT_BEAT_BROCK
+	setevent EVENT_BEAT_MISTY
+	setevent EVENT_BEAT_LTSURGE
+	setevent EVENT_BEAT_ERIKA
+	setevent EVENT_BEAT_JANINE
+	setevent EVENT_BEAT_SABRINA
+	setevent EVENT_BEAT_BLAINE
+	setevent EVENT_BEAT_BLUE
+	; fly anywhere
+	setflag ENGINE_FLYPOINT_NEW_BARK
+	setflag ENGINE_FLYPOINT_CHERRYGROVE
+	setflag ENGINE_FLYPOINT_VIOLET
+	setflag ENGINE_FLYPOINT_AZALEA
+	setflag ENGINE_FLYPOINT_GOLDENROD
+	setflag ENGINE_FLYPOINT_ECRUTEAK
+	setflag ENGINE_FLYPOINT_OLIVINE
+	setflag ENGINE_FLYPOINT_CIANWOOD
+	setflag ENGINE_FLYPOINT_MAHOGANY
+	setflag ENGINE_FLYPOINT_LAKE_OF_RAGE
+	setflag ENGINE_FLYPOINT_BLACKTHORN
+	setflag ENGINE_FLYPOINT_SILVER_CAVE
+	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
+	setflag ENGINE_FLYPOINT_PALLET
+	setflag ENGINE_FLYPOINT_VIRIDIAN
+	setflag ENGINE_FLYPOINT_PEWTER
+	setflag ENGINE_FLYPOINT_CERULEAN
+	setflag ENGINE_FLYPOINT_VERMILION
+	setflag ENGINE_FLYPOINT_CELADON
+	setflag ENGINE_FLYPOINT_ROCK_TUNNEL
+	setflag ENGINE_FLYPOINT_LAVENDER
+	setflag ENGINE_FLYPOINT_FUCHSIA
+	setflag ENGINE_FLYPOINT_SAFFRON
+	setflag ENGINE_FLYPOINT_CINNABAR
+	; good party
+	givepoke NIDOKING, 99, MIRACLEBERRY
+	givepoke ENTEI, 99, MIRACLEBERRY
+	; hm tools
+	givepoke MEW, 99, MIRACLEBERRY
+	givepoke KINGLER, 99, MIRACLEBERRY
+	loadmem wPartyMon1Moves+0, THUNDERBOLT
+	loadmem wPartyMon1Moves+1, ICE_BEAM
+	loadmem wPartyMon1Moves+2, EARTHQUAKE
+	loadmem wPartyMon1Moves+3, SURF
+	loadmem wPartyMon2Moves+0, FLAMETHROWER
+	loadmem wPartyMon2Moves+1, SUNNY_DAY
+	loadmem wPartyMon2Moves+2, SOLARBEAM
+	loadmem wPartyMon2Moves+3, IRON_TAIL
+	loadmem wPartyMon3Moves+0, FLY
+	loadmem wPartyMon3Moves+1, WHIRLPOOL
+	loadmem wPartyMon3Moves+2, HEADBUTT
+	loadmem wPartyMon3Moves+3, FLASH
+	loadmem wPartyMon4Moves+0, STRENGTH
+	loadmem wPartyMon4Moves+1, ROCK_SMASH
+	loadmem wPartyMon4Moves+2, CUT
+	loadmem wPartyMon4Moves+3, WATERFALL
+	; intro events
+	addcellnum PHONE_MOM
+	setmapscene PLAYERS_HOUSE_1F, $1
+	setevent EVENT_PLAYERS_HOUSE_MOM_1
+	clearevent EVENT_PLAYERS_HOUSE_MOM_2
 	closetext
-	setevent EVENT_LISTENED_TO_INITIAL_RADIO
 	end
 
 .NormalRadio:
